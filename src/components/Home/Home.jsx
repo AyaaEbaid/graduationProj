d3aa39234e7cf42ca5625a80707f5ff16382a23a
aya <aya@gmail.com>
2025-04-30 19:52:28 +0300

    updates

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import Background from '../Background/Background'
-import Aboutus from '../Aboutus/Aboutus'
-import Mainslider from '../Mainslider/Mainslider'
-import Services from '../Services/Services'
-import Achievements from '../Achievements/Achievements'
-import Footer from '../Footer/Footer'
-import GovernratesApi from '../GovernratesApi/GovernratesApi'
-
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Background from '../Background/Background';
+import Aboutus from '../Aboutus/Aboutus';
+import Mainslider from '../Mainslider/Mainslider';
+import Services from '../Services/Services';
+import Achievements from '../Achievements/Achievements';
+import Footer from '../Footer/Footer';
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [location]);
+
   return (
     <>
-    <Background/>
-    <Mainslider/>
-   
-    
-    <Aboutus />
-    
-    
-    <Services/>
-    <Achievements/>
-    
-    <Footer/>
-   
-    
+      <Background />
+      <Mainslider />
+      <div id="about-us">
+        <Aboutus />
+      </div>
+      <Services />
+      <Achievements />
+      <div id="contact-us">
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
