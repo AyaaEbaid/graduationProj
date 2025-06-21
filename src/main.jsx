dbef57741db174f4666bd90bb55f9f9644532a48
aya <aya@gmail.com>
2025-06-21 23:57:33 +0300

    new

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,13 @@ import TokenContextProvider from './Context/TokenContext.jsx';
 import { ImageCraftsmanProvider } from './Context/ImageCraftsmanContext.jsx';
 import { LocationProvider } from './Context/LocationContext.jsx';
 import { SpecializationProvider } from './Context/SpecailizationContext.jsx';
-import { UserProvider } from './Context/userContext.jsx';
+
 
 
 
 
 createRoot(document.getElementById('root')).render(
-<UserProvider>
+
  <LocationProvider>
   
  <SpecializationProvider>
@@ -29,7 +29,7 @@ createRoot(document.getElementById('root')).render(
   </ImageCraftsmanProvider>
   </SpecializationProvider>
   </LocationProvider>
-  </UserProvider>
+
   
   ,
 )
