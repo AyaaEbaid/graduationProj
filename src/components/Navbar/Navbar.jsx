db553a5eefa5be66d836d69a402d2b5c0268b1be
aya <aya@gmail.com>
2025-06-22 13:52:13 +0300

    Updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -134,15 +134,7 @@ export default function Navbar() {
         </div>
 
         <ul className="py-2" aria-labelledby="user-menu-button">
-          <li>
-            <NavLink
-              to="dashboard"
-              onClick={() => setDropdownOpen(false)}
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-            >
-              {t("navbar.dashboard")}
-            </NavLink>
-          </li>
+        
           <li>
             <NavLink
                to={
